4c19ae155cabcb70e07adb2d60fbeebc635632db
Danyal <mugutdinov.danyal@yandex.ru>
2019-03-08 17:54:51 -0500

    async version

--- request_loop.py
+++ request_loop.py
@@ -1,33 +1,20 @@
-import urllib.request as ur
-from urllib.error import *
-import http.client as l7client
-import sys
-
-def getRequest(url):
-    conn = ur.urlopen(url)
-    try:
-        data = conn.read()
-    except l7client.RemoteDisconnected:
-        print("can not connect")
-        sys.exit()
-
-    print(conn.status)
-    if conn.status == 200:
-        print(data)
-        return int(data);
+import grequests
 
 with open("filenames") as f:
     content = f.readlines()
 content = [x.strip() for x in content]
 
-total_time = 0
+urls = []
 for name in content:
     if name[0] == "#":
         continue
     url = "http://127.0.0.1:8080?path={}".format(name)
-    time = getRequest(url)
-    print(time)
-    total_time += time
+    urls.append(url)
 
-print("total execution time in msec ", total_time)
+rs = (grequests.get(u) for u in urls)
 
+total_time = 0
+for r in grequests.map(rs):
+    print(r.status_code, r.content)
+    total_time += int(r.content)
+print("total time: ", total_time)
